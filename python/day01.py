aaddc9dfb191399db9dc894b2c34094090319fad
Kevin Jones <kevin.jones@boldsci.com>
2022-12-01 10:49:41 +0000

    day1 refactor

--- python/day01.py
+++ python/day01.py
@@ -15,21 +15,21 @@ elf_calories = """1000
 
 10000"""
 
-def parse_raw_into_list_of_int_lists(lines_with_gaps):
+def convert_raw_to_lists_of_ints(lines_with_gaps):
     lines_with_gaps = lines_with_gaps.strip()
     list_lists = [line for line in lines_with_gaps.split("\n\n")]
     int_lists = [list(map(int, str_lists.split("\n"))) for str_lists in list_lists]
     return int_lists
 
 
-def day01a(elf_calories_raw):
-    elf_calories_list = parse_raw_into_list_of_int_lists(elf_calories_raw)
-    highest_count = max([sum(calory_list) for calory_list in elf_calories_list])
+def day01a(calories_lists_raw):
+    calories_lists = convert_raw_to_lists_of_ints(calories_lists_raw)
+    highest_count = max([sum(calory_list) for calory_list in calories_lists])
     return highest_count
 
-def day01b(elf_calories_raw):
-    elf_calories_list = parse_raw_into_list_of_int_lists(elf_calories_raw)
-    counts = [sum(calory_list) for calory_list in elf_calories_list]
+def day01b(calories_lists_raw):
+    calories_lists = convert_raw_to_lists_of_ints(calories_lists_raw)
+    counts = [sum(calory_list) for calory_list in calories_lists]
     return sum(sorted(counts, reverse=True)[:3])
 
 def test_01a_ex0(): 
