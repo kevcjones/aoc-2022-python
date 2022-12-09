54757c1bb97fbea6ebaf9a2376d15771451fcc71
Kevin Jones <kevin.jones@boldsci.com>
2022-12-09 10:57:45 +0000

    day9

---
+++ python/benchmark.py
@@ -0,0 +1,32 @@
+from python.day01 import test_day01_part1 as _test_day01_part1
+def test_day01_part1_(day01_raw, benchmark): benchmark(lambda: _test_day01_part1(day01_raw))
+from python.day01 import test_day01_part2 as _test_day01_part2
+def test_day01_part2_(day01_raw, benchmark): benchmark(lambda: _test_day01_part2(day01_raw))
+from python.day02 import test_day02_part1 as _test_day02_part1
+def test_day02_part1_(day02_raw, benchmark): benchmark(lambda: _test_day02_part1(day02_raw))
+from python.day02 import test_day02_part2 as _test_day02_part2
+def test_day02_part2_(day02_raw, benchmark): benchmark(lambda: _test_day02_part2(day02_raw))
+from python.day03 import test_day03_part1 as _test_day03_part1
+def test_day03_part1_(day03_raw, benchmark): benchmark(lambda: _test_day03_part1(day03_raw))
+from python.day03 import test_day03_part2 as _test_day03_part2
+def test_day03_part2_(day03_raw, benchmark): benchmark(lambda: _test_day03_part2(day03_raw))
+from python.day04 import test_day04_part1 as _test_day04_part1
+def test_day04_part1_(day04_raw, benchmark): benchmark(lambda: _test_day04_part1(day04_raw))
+from python.day04 import test_day04_part2 as _test_day04_part2
+def test_day04_part2_(day04_raw, benchmark): benchmark(lambda: _test_day04_part2(day04_raw))
+from python.day05 import test_day05_part1 as _test_day05_part1
+def test_day05_part1_(day05_raw, benchmark): benchmark(lambda: _test_day05_part1(day05_raw))
+from python.day05 import test_day05_part2 as _test_day05_part2
+def test_day05_part2_(day05_raw, benchmark): benchmark(lambda: _test_day05_part2(day05_raw))
+from python.day06 import test_day06_part1 as _test_day06_part1
+def test_day06_part1_(day06_raw, benchmark): benchmark(lambda: _test_day06_part1(day06_raw))
+from python.day06 import test_day06_part2 as _test_day06_part2
+def test_day06_part2_(day06_raw, benchmark): benchmark(lambda: _test_day06_part2(day06_raw))
+from python.day07 import test_day07_part1 as _test_day07_part1
+def test_day07_part1_(day07_raw, benchmark): benchmark(lambda: _test_day07_part1(day07_raw))
+from python.day07 import test_day07_part2 as _test_day07_part2
+def test_day07_part2_(day07_raw, benchmark): benchmark(lambda: _test_day07_part2(day07_raw))
+from python.day08 import test_day08_part1 as _test_day08_part1
+def test_day08_part1_(day08_raw, benchmark): benchmark(lambda: _test_day08_part1(day08_raw))
+from python.day08 import test_day08_part2 as _test_day08_part2
+def test_day08_part2_(day08_raw, benchmark): benchmark(lambda: _test_day08_part2(day08_raw))
---
+++ python/day09.py
@@ -0,0 +1,47 @@
+import pytest
+from utils.examples import read_raw_example_for_day
+import math
+
+@pytest.fixture
+def day09_example_raw():
+    return read_raw_example_for_day(9)
+
+def convert(raw):
+    raw = raw.strip()
+    offsets = {'L': (-1, 0), 'R': (1, 0), 'D': (0, -1), 'U': (0, 1)}
+    casted = [ (offsets[line[0]], int(line[1:])) for line in raw.split("\n")]
+    return casted
+
+
+
+def move(moves, length):
+    xs = [0] * length
+    ys = [0] * length
+    visited = { (xs[-1], ys[-1]) }
+    for (mx, my), distance in moves:
+        for _ in range(distance):
+            xs[0] += mx
+            ys[0] += my
+            for i in range(length - 1):
+                dx = xs[i + 1] - xs[i]
+                dy = ys[i + 1] - ys[i]
+                if abs(dx) == 2 or abs(dy) == 2:
+                    xs[i + 1] = xs[i] + int(dx / 2)
+                    ys[i + 1] = ys[i] + int(dy / 2)
+            visited.add( (xs[-1], ys[-1]) )
+    return len(visited)
+
+def day09_part1(raw):
+    moves = convert(raw)
+    return move(moves, 2)
+
+def day09_part2(raw):
+    moves = convert(raw)
+    return move(moves, 10)
+
+
+def test_day09_part1(day09_raw): 
+    assert day09_part1(day09_raw) == 6339
+
+def test_day09_part2(day09_raw): 
+    assert day09_part2(day09_raw) == 2541
