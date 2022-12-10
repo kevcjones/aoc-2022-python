5dcd16c0ce286ad79a6be6e88c957fde37bfec6f
Kevin Jones <kevin.jones@boldsci.com>
2022-12-10 20:48:21 +0000

    day10 refactored final

--- python/day10.py
+++ python/day10.py
@@ -1,41 +1,39 @@
 import pytest
 from utils.examples import read_raw_example_for_day
-import math
+from functools import reduce
 
 @pytest.fixture
 def day10_example_raw():
     return read_raw_example_for_day(10)
 
+def process_instructions(acc, op):
+    value, sums = acc
+    if op == 'noop':
+        sums.append(value)
+    else:
+        _,add = op.split()
+        sums.append(value)
+        value += int(add)
+        sums.append(value)
+    return (value, sums)
+
 def convert(raw):
     input = raw.split('\n')
-    value = 1
-    summed_taly = [1]
-    for op in input:
-        if op == 'noop':
-            summed_taly.append(value)
-        else:
-            parts = op.split()
-            if parts[0] == 'addx':
-                summed_taly.append(value)
-                value += int(parts[1])
-                summed_taly.append(value)
-    return summed_taly
+    _,sums = reduce(process_instructions, input, (1, [1]))
+    return sums
 
-def process_part1(cycles: list):
-    return sum(cycle * value for cycle, value in zip(cycles[19:220:40], [20, 60, 100, 140, 180, 220]))
+def day10_part1(raw):
+    sums = convert(raw)
+    return sum(cycle * value for cycle, value in zip(sums[19:220:40], [20, 60, 100, 140, 180, 220]))
 
-def process_part2(buffer: list):
+def day10_part2(raw):
+    buffer = convert(raw)
     line = 40
     crt = ['█'  if abs(pos - (i % line)) <= 1 else '.' for i, pos in enumerate(buffer)]
     return [''.join(crt[i * line:(i + 1) * line]) for i in range(0, len(buffer) // line)]
 
-def day10_part1(raw):
-    cycles = convert(raw)
-    return process_part1(cycles)
 
-def day10_part2(raw):
-    taly = convert(raw)
-    return process_part2(taly)
+# tests
 
 def test_day10_part1_example(day10_example_raw): 
     assert day10_part1(day10_example_raw) == 13140
